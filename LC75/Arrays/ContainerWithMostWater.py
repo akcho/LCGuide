412ed4a889ab23e130d11b138b37f970d67eb336
Andrew Cho <andrew@augmentedrealitycapture.com>
2021-10-06 12:46:25 -0700

    big array commit

---
+++ LC75/Arrays/ContainerWithMostWater.py
@@ -0,0 +1,16 @@
+# Time: O(n)
+# Space: O(1)
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        max_area = 0
+        l = 0
+        r = len(height) - 1
+
+        while (l < r):
+            max_area = max(max_area, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return max_area
