426dadc01a6626797a2e2d56a85e6ec2e795c194
Andrew Cho <andrew@augmentedrealitycapture.com>
2021-12-28 10:25:31 -0800

    Update ConstructBinaryTreeFromInorderAndPostorderTraversal

--- Explore/Binary Trees/Conclusion/ConstructBinaryTreeFromInorderAndPostorderTraversal.py
+++ Explore/Binary Trees/Conclusion/ConstructBinaryTreeFromInorderAndPostorderTraversal.py
@@ -1,12 +1,21 @@
+# time: O(n)
+# space: O(n) >- store the entire tree
 class Solution:
-    def buildTree(self, inorder, postorder):
-        map_inorder = {}
-        for i, val in enumerate(inorder): map_inorder[val] = i
-        def recur(low, high):
-            if low > high: return None
-            x = TreeNode(postorder.pop())
-            mid = map_inorder[x.val]
-            x.right = recur(mid+1, high)
-            x.left = recur(low, mid-1)
-            return x
-        return recur(0, len(inorder)-1)
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        def helper(in_left, in_right):
+            if in_left > in_right:
+                return None
+
+            val = postorder.pop()
+            root = TreeNode(val)
+
+            index = idx_map[val]
+
+            root.right = helper(index + 1, in_right)
+            root.left = helper(in_left, index - 1)
+
+            return root
+
+        idx_map = {val: idx for idx, val in enumerate(inorder)}
+        print(idx_map)
+        return helper(0, len(inorder) - 1)
