5b5b8db45cbaa41fa1fe819708470522a15740dc
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-03 17:12:24 -0800

    commit

--- Explore/Binary Trees/Solve Problems Recursively/PathSum.py
+++ Explore/Binary Trees/Solve Problems Recursively/PathSum.py
@@ -1,3 +1,4 @@
+# Approach 1: Recursion
 # time: O(n)
 # space: O(log(n))
 
@@ -12,3 +13,28 @@ class Solution:
             return targetSum == 0
 
         return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+
+
+# Approach 2: Iteration (DFS)
+# time: O(n)
+# space (O(log(n))
+class Solution:
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+
+        de = [(root, sum - root.val), ]
+        while de:
+            node, curr_sum = de.pop()
+            if not node.left and not node.right and curr_sum == 0:
+                return True
+            if node.right:
+                de.append((node.right, curr_sum - node.right.val))
+            if node.left:
+                de.append((node.left, curr_sum - node.left.val))
+        return False
