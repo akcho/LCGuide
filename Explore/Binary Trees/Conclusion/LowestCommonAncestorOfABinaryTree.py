9fb9fa892f4b5efaa4db0413a7ed7350730eaefb
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-05 15:37:34 -0800

    commit

--- Explore/Binary Trees/Conclusion/LowestCommonAncestorOfABinaryTree.py
+++ Explore/Binary Trees/Conclusion/LowestCommonAncestorOfABinaryTree.py
@@ -1,14 +1,13 @@
 # time: O(n)
 # space: O(n)
 class Solution:
-    def lowestCommonAncestor(self, root, p, q):
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         def LCA(root):
-            if root is None or root in (p, q):
+            if not root or (root in (p, q)):
                 return root
             left = LCA(root.left)
             right = LCA(root.right)
             if left and right:
                 return root
-            else:  # one of them is null, so return the good one
-                return left or right
+            return left or right  # only possible bc p and q HAVE to be in root
         return LCA(root)
---
+++ Explore/Binary Trees/Conclusion/SerializeAndDeserializeBinaryTree.py
@@ -0,0 +1,35 @@
+# Recursive DFS
+# time: O(n)
+# space: O(n)
+class Codec:
+
+    def serialize(self, root):
+        res = []
+
+        def dfs(root):
+            if not root:
+                res.append("None,")
+                return
+            res.append(str(root.val) + ",")
+            dfs(root.left)
+            dfs(root.right)
+
+        dfs(root)
+        return "".join(res)
+
+    def deserialize(self, data):
+        def dfs(str_q):
+            if str_q[0] == "None":
+                str_q.popleft()
+                return None
+            root = TreeNode(str_q[0])
+            str_q.popleft()
+            root.left = dfs(str_q)
+            root.right = dfs(str_q)
+            return root
+
+        data_list = data.split(',')
+        data_q = deque(data_list)
+        root = dfs(data_q)
+        return root
+
