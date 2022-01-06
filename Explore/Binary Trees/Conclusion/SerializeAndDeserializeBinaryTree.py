ef7601785e766bcc8d5dd5e68f75953fddede43d
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-05 16:25:52 -0800

    commit

--- Explore/Binary Trees/Conclusion/SerializeAndDeserializeBinaryTree.py
+++ Explore/Binary Trees/Conclusion/SerializeAndDeserializeBinaryTree.py
@@ -2,34 +2,27 @@
 # time: O(n)
 # space: O(n)
 class Codec:
-
     def serialize(self, root):
         output = []
-
         def dfs(root):
             if not root:
                 output.append("None,")
-                return
+                return None
             output.append(str(root.val) + ",")
             dfs(root.left)
             dfs(root.right)
-
         dfs(root)
-        return "".join(res)
+        return "".join(output)
 
     def deserialize(self, data):
-        def dfs(str_q):
-            if str_q[0] == "None":
-                str_q.popleft()
-                return None
-            root = TreeNode(str_q[0])
-            str_q.popleft()
-            root.left = dfs(str_q)
-            root.right = dfs(str_q)
-            return root
-
-        data_list = data.split(',')
+        data_list = data.split(",")
         data_q = deque(data_list)
-        root = dfs(data_q)
-        return root
-
+        def dfs(q):
+            val = q.popleft()
+            if val == "None":
+                return None
+            root = TreeNode(val)
+            root.left = dfs(q)
+            root.right = dfs(q)
+            return root
+        return dfs(data_q)
