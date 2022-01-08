8523c2c8e3e57caf593acffbddf94a1d059b4e7a
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-07 17:33:57 -0800

    commit

--- Explore/Queue and Stack/DesignCircularQueue.py
+++ Explore/Queue and Stack/DesignCircularQueue.py
@@ -1,43 +1,45 @@
-class MyCircularQueue:
-    def __init__(self, k: int):
-        self.queue = [0] * k
-        self.head = 0
-        self.filled_slots = 0  # how many slots in q are actually being used
-        self.fixed_size = k  # fixed queue size (set by k)
+# time: O(1)
+# space: O(n)
 
-    def setTail(self):
-        return ((self.head + self.filled_slots) % self.fixed_size) - 1
+from threading import Lock
+
+
+class MyCircularQueue:
+
+    def __init__(self, k: int):
+        self.q = [0] * k
+        self.capacity = k
+        self.head = 0
+        self.filled_slots = 0
+        self.q_lock = Lock()
+
+    def getTail(self):
+        return ((self.head + self.filled_slots) % self.capacity) - 1
 
     def enQueue(self, value: int) -> bool:
-        if self.filled_slots == self.fixed_size:
-            return False
-        self.filled_slots += 1
-        tail = self.setTail()
-        self.queue[tail] = value
+        with self.q_lock:
+            if self.isFull(): return False
+            self.filled_slots += 1
+            self.q[self.getTail()] = value
         return True
 
     def deQueue(self) -> bool:
-        if self.filled_slots == 0:
-            return False
-        self.head = (self.head + 1) % self.fixed_size
+        if self.isEmpty(): return False
+        self.head = (self.head + 1) % self.capacity
         self.filled_slots -= 1
         return True
 
     def Front(self) -> int:
-        if self.filled_slots == 0:
-            return -1
-        return self.queue[self.head]
+        if self.isEmpty(): return -1
+        return self.q[self.head]
 
     def Rear(self) -> int:
-        if self.filled_slots == 0:
-            return -1
-        tail = self.setTail()
-        return self.queue[tail]
+        if self.isEmpty(): return -1
+        return self.q[self.getTail()]
 
     def isEmpty(self) -> bool:
         return self.filled_slots == 0
 
     def isFull(self) -> bool:
-        return self.filled_slots == self.fixed_size
+        return self.filled_slots == self.capacity
 
-    
