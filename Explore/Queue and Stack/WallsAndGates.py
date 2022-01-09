44c49cde7426429e8aeb1037ef00cbe8262bd3e3
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-08 21:04:44 -0800

    commit

--- Explore/Queue and Stack/WallsAndGates.py
+++ Explore/Queue and Stack/WallsAndGates.py
@@ -3,30 +3,34 @@
 # space: O(mn)
 class Solution:
     def wallsAndGates(self, rooms: List[List[int]]) -> None:
+        if not rooms: return
+
         EMPTY = 2 ** 31 - 1
         GATE = 0
         DIRECTIONS = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         rows = len(rooms)
-        if rows == 0: return
         cols = len(rooms[0])
         q = deque()
 
+        # queue starts with gate coords but will also include revised (prev empty) cells
         for row in range(rows):
             for col in range(cols):
                 # add gate to queue
                 if rooms[row][col] == GATE:
                     q.append([row, col])
 
+        # BFS
         while len(q) != 0:
-            # pop gate coords off
-            coord = q.popleft()
-            row = coord[0]  # 1 is north, -1 is south
-            col = coord[1]  # 1 is east, -1 is west
+            popped_row, popped_col = q.popleft()
             for direction in DIRECTIONS:
-                r = row + direction[0]
-                c = col + direction[1]
-                if r < 0 or c < 0 or r >= rows or c >= cols or rooms[r][c] != EMPTY:
-                    continue
-                rooms[r][c] = rooms[row][col] + 1
-                q.append([r, c])
+                r = popped_row + direction[0]
+                c = popped_col + direction[1]
+                if 0 <= r < rows and 0 <= c < cols and rooms[r][c] == EMPTY:
+                    rooms[r][c] = rooms[popped_row][popped_col] + 1
+                    q.append([r, c])
+
+
+
+
+
