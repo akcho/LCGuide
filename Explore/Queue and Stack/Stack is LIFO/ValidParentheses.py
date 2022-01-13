4d93170320b67d8022da526d5a82bb7c683c8a5b
Andrew Cho <acho41@gatech.edu>
2022-01-13 13:12:50 -0800

    commit

---
+++ Explore/Queue and Stack/Stack is LIFO/ValidParentheses.py
@@ -0,0 +1,17 @@
+# basic stack
+# time: O(n)
+# space: O(n)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        for char in s:
+            if char in mapping:
+                top_element = stack.pop() if stack else "-1"
+                if mapping[char] != top_element:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
+
