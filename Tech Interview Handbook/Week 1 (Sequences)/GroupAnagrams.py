746d79acbbb61ba8c1bb6e3b1d5031c65080be54
Andrew Cho <acho41@gatech.edu>
2022-01-15 12:04:13 -0800

    commit

---
+++ Tech Interview Handbook/Week 1 (Sequences)/GroupAnagrams.py
@@ -0,0 +1,13 @@
+# default dict
+# Time Complexity: O(NK), where N is the length of strs, and K is the maximum length of a string in strs.
+#Space Complexity: O(NK), the total information content stored in ans.
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
