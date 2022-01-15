80299812d7cf0b79bf6d1f8936ecb587bc48dfd6
Andrew Cho <acho41@gatech.edu>
2022-01-14 21:14:08 -0800

    commit

---
+++ Tech Interview Handbook/Week 1 (Sequences)/ValidAnagram.py
@@ -0,0 +1,23 @@
+# hashmap
+# time: O(n)
+# space: O(n)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hm = {}
+
+        for char in s:
+            if char not in hm:
+                hm[char] = 1
+            else:
+                hm[char] += 1
+
+        for char in t:
+            if char not in hm:
+                return False
+            else:
+                hm[char] -= 1
+
+        for h in hm:
+            if hm[h] != 0: return False
+        return True
