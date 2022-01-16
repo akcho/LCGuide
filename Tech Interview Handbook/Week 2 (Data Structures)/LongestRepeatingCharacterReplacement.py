11d02600694d9c7fe832795339ce3f15ef04b6d3
Andrew Cho <acho41@gatech.edu>
2022-01-15 18:32:54 -0800

    commit

--- Tech Interview Handbook/Week 2 (Data Structures)/LongestRepeatingCharacterReplacement.py
+++ Tech Interview Handbook/Week 2 (Data Structures)/LongestRepeatingCharacterReplacement.py
@@ -6,19 +6,21 @@ class Solution:
         # Max_length the longest subsequence without repeating chars and k changes
         # Max_count is the highest count of chars in the answer subsequence
         max_length = max_count = 0
-        # Count keeps track of the chars in the we are looking at subsequence
+        # count keeps track of all of the chars that we are looking at in subsequence
         count = defaultdict(int)
         for i in range(len(s)):
             # Add char to the count dict
             count[s[i]] += 1
-            # key idea(2): Find the new max_count. This is much like Kadane's
-            # Where we only consider if the new length exceedes the max_length overall
+
+            # Find the new max_count. This is much like Kadane's,
+            # where we only consider if the new length exceeds the max_length.
             max_count = max(max_count, count[s[i]])
-            # Key idea (1): the answer is always max_count + k.
+
+            # The answer must always be max_count + k.
             if max_length < k + max_count:
                 max_length += 1
             else:
-                # key idea(3) This removes the char at the start of the subsequence s[i-max_length]
-                # This serves as "correction" for the subsequence problem
+                # s[i-max_length] removes the char at the start of a following subsequence:
+                # This serves as the "correction" for a subsequence problem
                 count[s[i-max_length]] -= 1
         return max_length
