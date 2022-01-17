86065d04977068ba53522c5fdcaa8576fc979ffb
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-17 15:11:05 -0800

    commit

--- Tech Interview Handbook/Week 2 (Data Structures)/LongestRepeatingCharacterReplacement.py
+++ Tech Interview Handbook/Week 2 (Data Structures)/LongestRepeatingCharacterReplacement.py
@@ -1,27 +1,20 @@
 # time: O(n)
-# space: O(1) -> 26, going through each char
+# space: O(n) -> 26, if we have to store every char in defaultdict
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        # Max_length is the longest overall subsequence (final answer)
-        # Max_count is just the highest count of chars within the curr subsequence
-        max_length = max_count = 0
-        # count keeps track of all of the chars that we are looking at in the subsequence
+        window_size = 0
+        max_count = 0
         count = defaultdict(int)
+
         for i in range(len(s)):
             c = s[i]
-            # Add char to the count dict
             count[c] += 1
-
-            # Find the new max_count. This is much like Kadane's,
-            # where we only consider if the new length exceeds the max_length.
             max_count = max(max_count, count[c])
 
-            # The answer must always be max_count + k.
-            if max_length < k + max_count:
-                max_length += 1
+            if k > window_size - max_count:
+                window_size += 1
             else:
-                # s[i-max_length] removes the char at the start of a following subsequence:
-                # This serves as the "correction" for a subsequence problem
-                count[s[i-max_length]] -= 1
-        return max_length
+                # subtract window start's count to slide the window
+                count[s[i - window_size]] -= 1
+        return window_size
