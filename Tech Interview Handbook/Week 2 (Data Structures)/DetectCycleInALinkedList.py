3ce0bba82633b42a6f15ece547f659b394a2aea3
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-24 22:07:17 -0800

    commit

--- Tech Interview Handbook/Week 2 (Data Structures)/DetectCycleInALinkedList.py
+++ Tech Interview Handbook/Week 2 (Data Structures)/DetectCycleInALinkedList.py
@@ -1,4 +1,4 @@
-# let's just stick to hash map, bc two pointers seems too specific
+# hash map
 # time: O(n)
 # space: O(n)
 
@@ -12,3 +12,19 @@ class Solution:
             node_record.add(head)
             head = head.next
         return False
+
+# Floyd's Cycle Finding Algorithm (adding here bc it's a cool solution, but I'd probably go with the first)
+# time: O(n)
+# space: O(1)
+class Solution2:
+    def hasCycle(self, head: ListNode) -> bool:
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        return True
