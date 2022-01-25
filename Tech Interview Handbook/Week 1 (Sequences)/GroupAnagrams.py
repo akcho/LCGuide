bc03fb3ebfa0ebc1b01a760920b079134404a9e5
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-24 19:04:42 -0800

    commit

--- Tech Interview Handbook/Week 1 (Sequences)/GroupAnagrams.py
+++ Tech Interview Handbook/Week 1 (Sequences)/GroupAnagrams.py
@@ -1,6 +1,6 @@
 # default dict
 # Time Complexity: O(NK), where N is the length of strs, and K is the maximum length of a string in strs.
-#Space Complexity: O(NK), the total information content stored in ans.
+# Space Complexity: O(NK), the total information content stored in ans.
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
