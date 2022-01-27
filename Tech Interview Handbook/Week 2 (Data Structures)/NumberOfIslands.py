bc064ca9d44a5cc6b86d957bf34bc5c24d55986b
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-26 21:52:02 -0800

    commit

--- Tech Interview Handbook/Week 2 (Data Structures)/NumberOfIslands.py
+++ Tech Interview Handbook/Week 2 (Data Structures)/NumberOfIslands.py
@@ -14,36 +14,36 @@ class Solution:
         num_cols = len(grid[0])
 
         num_islands = 0
-        for r in range(num_rows):
-            for c in range(num_cols):
-                if grid[r][c] == LAND:
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if grid[row][col] == LAND:
                     num_islands += 1
-                    grid[r][c] = VISITED
+                    grid[row][col] = VISITED
                     neighbors = deque()
-                    neighbors.append([r, c])
+                    neighbors.append([row, col])
                     while neighbors:
-                        row, col = neighbors.popleft()
-                        up = row - 1
-                        down = row + 1
-                        left = col - 1
-                        right = col + 1
+                        n_row, n_col = neighbors.popleft()
+                        up = n_row - 1
+                        down = n_row + 1
+                        left = n_col - 1
+                        right = n_col + 1
 
                         up_in_bounds = (up >= 0)
                         down_in_bounds = (down < num_rows)
                         left_in_bounds = (left >= 0)
                         right_in_bounds = (right < num_cols)
 
-                        if up_in_bounds and grid[up][col] == LAND:
-                            neighbors.append([up, col])
-                            grid[up][col] = VISITED
-                        if down_in_bounds and grid[down][col] == LAND:
-                            neighbors.append([down, col])
-                            grid[down][col] = VISITED
-                        if left_in_bounds and grid[row][left] == LAND:
-                            neighbors.append([row, left])
-                            grid[row][left] = VISITED
-                        if right_in_bounds and grid[row][right] == LAND:
-                            neighbors.append([row, right])
-                            grid[row][right] = VISITED
+                        if up_in_bounds and grid[up][n_col] == LAND:
+                            neighbors.append([up, n_col])
+                            grid[up][n_col] = VISITED
+                        if down_in_bounds and grid[down][n_col] == LAND:
+                            neighbors.append([down, n_col])
+                            grid[down][n_col] = VISITED
+                        if left_in_bounds and grid[n_row][left] == LAND:
+                            neighbors.append([n_row, left])
+                            grid[n_row][left] = VISITED
+                        if right_in_bounds and grid[n_row][right] == LAND:
+                            neighbors.append([n_row, right])
+                            grid[n_row][right] = VISITED
         return num_islands
 
