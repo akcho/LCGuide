0b6cc7a78537eb1ce611bda1de20daabfd4f59bf
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-01-27 23:48:14 -0800

    commit

---
+++ Tech Interview Handbook/Week 3 (Non-linear Data Structures)/SerializeAndDeserializeBinaryTree.py
@@ -0,0 +1,28 @@
+# Recursive DFS
+# time: O(n)
+# space: O(n)
+class Codec:
+    def serialize(self, root):
+        output = []
+        def dfs(root):
+            if not root:
+                output.append("None,")
+                return None
+            output.append(str(root.val) + ",")
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        return "".join(output)
+
+    def deserialize(self, data):
+        data_list = data.split(",")
+        data_q = deque(data_list)
+        def dfs(q):
+            val = q.popleft()
+            if val == "None":
+                return None
+            root = TreeNode(val)
+            root.left = dfs(q)
+            root.right = dfs(q)
+            return root
+        return dfs(data_q)
