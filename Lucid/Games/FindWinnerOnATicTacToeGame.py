847db82efa84ac55a781d0865db89c96dad83d11
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-12 16:32:17 -0800

    commit

--- Lucid/Games/FindWinnerOnATicTacToeGame.py
+++ Lucid/Games/FindWinnerOnATicTacToeGame.py
@@ -4,21 +4,21 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
         n = 3
-        rows, cols = [0] * n, [0] * n
+        row_sums, col_sums = [0] * n, [0] * n
         diag = anti_diag = 0
 
         player = 1
 
         for row, col in moves:
-            rows[row] += player
-            cols[col] += player
+            row_sums[row] += player
+            col_sums[col] += player
 
             if row == col:
                 diag += player
             if row + col == n - 1:
                 anti_diag += player
 
-            for line in (rows[row], cols[col], diag, anti_diag):
+            for line in (row_sums[row], col_sums[col], diag, anti_diag):
                 if abs(line) == n:
                     return "A" if player == 1 else "B"
 
