44a1c0cad09f1ba9518712d84ef39b145ca0aaa9
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-16 16:30:13 -0800

    commit

---
+++ Tech Interview Handbook/Week 4 (More Data Structures)/LowestCommonAncestorOfABinaryTree.py
@@ -0,0 +1,14 @@
+# recursive DFS
+# time: O(H), h = height of tree, which means best-case is O(log n) and worst-case is O(n)
+# space: O(n)
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def LCA(root):
+            if not root or (root in (p, q)):
+                return root
+            left = LCA(root.left)
+            right = LCA(root.right)
+            if left and right:
+                return root
+            return left or right  # only possible bc p and q HAVE to be in root
+        return LCA(root)
