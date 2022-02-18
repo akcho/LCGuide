68f7009a2b3d73572237473999dfda9b819123c7
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-17 19:23:27 -0800

    commit

--- Tech Interview Handbook/Week 2 (Data Structures)/NumberOfIslands.py
+++ Tech Interview Handbook/Week 2 (Data Structures)/NumberOfIslands.py
@@ -1,3 +1,34 @@
+# Recursive DFS
+# time: O(m*n)
+# space: O(1)
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        LAND = "1"
+        WATER = "0"
+        VISITED = "-1"
+        num_rows, num_cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if (not 0 <= r < num_rows or
+                    not 0 <= c < num_cols or
+                    grid[r][c] != LAND):
+                return
+
+            grid[r][c] = VISITED
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        num_islands = 0
+        for r in range(num_rows):
+            for c in range(num_cols):
+                if grid[r][c] == LAND:
+                    num_islands += 1
+                    dfs(r, c)
+
+        return num_islands
+
 # BFS
 # time: O(mn)
 # space: O(min(m,n))
