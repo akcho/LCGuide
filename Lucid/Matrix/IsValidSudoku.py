cc9887f6679fb207c5e467df56d7be37c5cb08a2
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-22 12:25:09 -0800

    commit

--- Lucid/Matrix/IsValidSudoku.py
+++ Lucid/Matrix/IsValidSudoku.py
@@ -3,18 +3,22 @@
 # space: O(m * n)
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        cols = defaultdict(set)
         rows = defaultdict(set)
-        squares = defaultdict(set)  # key = (r//3,c//3)
+        cols = defaultdict(set)
+        squares = defaultdict(set)
+
+        num_rows, num_cols = len(board), len(board[0])
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == '.': continue
-                if (board[r][c] in cols[c] or
-                        board[r][c] in rows[r] or
-                        board[r][c] in squares[(r // 3, c // 3)]): return False
 
-                cols[c].add(board[r][c])
+                if board[r][c] in rows[r]: return False
+                if board[r][c] in cols[c]: return False
+                if board[r][c] in squares[(r // 3, c // 3)]: return False
+
                 rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
                 squares[(r // 3, c // 3)].add(board[r][c])
+
         return True
