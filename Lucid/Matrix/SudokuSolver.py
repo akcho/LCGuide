cbb873237d6c68571e7efa7496a311f3321b9272
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-22 21:46:32 -0800

    commit

--- Lucid/Matrix/SudokuSolver.py
+++ Lucid/Matrix/SudokuSolver.py
@@ -3,35 +3,37 @@
 # space: O(81)
 
 class Solution:
-    def solveSudoku(self, board: 'List[List[str]]') -> 'None':
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         self.board = board
         self.solve()
 
-    def findUnassigned(self):
-        for row in range(9):
-            for col in range(9):
-                if self.board[row][col] == '.':
-                    return row, col
+    def find_unassigned(self):
+        for r in range(9):
+            for c in range(9):
+                if self.board[r][c] == '.': return r, c
         return -1, -1
 
-    def solve(self):
-        row, col = self.findUnassigned()
-        if (row, col) == (-1, -1):
-            return True
-
-        for num in map(str, range(1, 10)):
-            if self.isSafe(row, col, num):
-                self.board[row][col] = num
-                if self.solve():
-                    return True
-                self.board[row][col] = '.'
-
-    def isSafe(self, row, col, ch):
-        rowSafe = all(self.board[row][_] != ch for _ in range(9))
-        colSafe = all(self.board[_][col] != ch for _ in range(9))
-        squareSafe = all(self.board[r][c] != ch for r in self.getRange(row) for c in self.getRange(col))
-        return rowSafe and colSafe and squareSafe
-
-    def getRange(self, x):
+    def get_range(self, x):
         x -= x % 3
         return range(x, x + 3)
+
+    def is_safe(self, r, c, num):
+        row_safe = all(self.board[r][i] != num for i in range(9))
+        col_safe = all(self.board[i][c] != num for i in range(9))
+        square_safe = all(self.board[i][j] != num for i in self.get_range(r) for j in self.get_range(c))
+        return row_safe and col_safe and square_safe
+
+    def solve(self):
+        r, c = self.find_unassigned()
+        if (r, c) == (-1, -1): return True
+
+        for num in map(str, range(1, 10)):
+            if self.is_safe(r, c, num):
+                self.board[r][c] = num
+                if self.solve():
+                    return True
+                else:
+                    self.board[r][c] = '.'
