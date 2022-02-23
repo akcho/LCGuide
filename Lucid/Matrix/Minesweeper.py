c606e182d0720e37dcec132eb8ecb7ffdc696795
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-23 12:57:38 -0800

    commit

--- Lucid/Matrix/Minesweeper.py
+++ Lucid/Matrix/Minesweeper.py
@@ -1,7 +1,6 @@
 # DFS
 # time: O(1)
 # space: O(1)
-
 class Solution:
     def updateBoard(self, board: List[List[str]], click: List[int]) -> List[List[str]]:
         num_rows, num_cols = len(board), len(board[0])
@@ -21,22 +20,19 @@ class Solution:
             return num_mines
 
         def dfs(r, c):
-            if board[r][c] != 'E': return  # check that it hasn't already been visited
+            if board[r][c] != 'E': return
 
             num_mines = get_adjacent_mines(r, c)
-            if num_mines == 0:
-                board[r][c] = 'B'
-            else:
+            if num_mines > 0:
                 board[r][c] = str(num_mines)
                 return
-
-            for nr in range(r - 1, r + 2):
-                for nc in range(c - 1, c + 2):
-                    if 0 <= nr < num_rows and 0 <= nc < num_cols:
-                        dfs(nr, nc)
+            else:
+                board[r][c] = 'B'
+                for nr in range(r - 1, r + 2):
+                    for nc in range(c - 1, c + 2):
+                        if 0 <= nr < num_rows and 0 <= nc < num_cols:
+                            dfs(nr, nc)
 
         dfs(r, c)
         return board
-
-
 
