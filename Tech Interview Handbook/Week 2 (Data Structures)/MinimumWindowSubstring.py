c36961856e48cd4e6f54fbf8bca3377d185ad2a2
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-28 12:08:53 -0800

    commit

--- Tech Interview Handbook/Week 2 (Data Structures)/MinimumWindowSubstring.py
+++ Tech Interview Handbook/Week 2 (Data Structures)/MinimumWindowSubstring.py
@@ -3,37 +3,34 @@
 # space: O(s + t)
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        chars_still_needed = Counter(t)
-        left, right = 0, 0
+        min_win_start = 0
         PLACEHOLDER_VAL = len(s) + 1
         min_win_length = PLACEHOLDER_VAL
-        min_win_start = 0
-        total_chars_still_needed = len(t)
+        left = 0
+        right = 0
+        total_chars_left = len(t)
+        chars_left = Counter(t)
 
         while right < len(s):
             right_char = s[right]
-            if right_char in chars_still_needed:
-                chars_still_needed[right_char] -= 1
-                if chars_still_needed[right_char] >= 0:
-                    total_chars_still_needed -= 1
+            if right_char in chars_left:
+                if chars_left[right_char] > 0:
+                    total_chars_left -= 1
+                chars_left[right_char] -= 1
 
-
-            while total_chars_still_needed == 0:
+            while total_chars_left == 0:
                 curr_win_length = right - left + 1
                 if curr_win_length < min_win_length:
                     min_win_length = curr_win_length
                     min_win_start = left
 
                 left_char = s[left]
-                if left_char in chars_still_needed:
-                    chars_still_needed[left_char] += 1
-                    if chars_still_needed[left_char] > 0:
-                        total_chars_still_needed += 1
+                if left_char in chars_left:
+                    chars_left[left_char] += 1
+                    if chars_left[left_char] > 0:
+                        total_chars_left += 1
                 left += 1
-
             right += 1
 
-        if min_win_length == len(s) + 1:
-            return ""
-        else:
-            return s[min_win_start:min_win_start + min_win_length]
+        if min_win_length == PLACEHOLDER_VAL: return ""
+        return s[min_win_start:min_win_start + min_win_length]
