0f9011fa33d19fd0c476277d735c4ddc61d7aaed
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-02-27 22:07:28 -0800

    commit

--- Tech Interview Handbook/Week 2 (Data Structures)/PacificAtlanticWaterFlow.py
+++ Tech Interview Handbook/Week 2 (Data Structures)/PacificAtlanticWaterFlow.py
@@ -5,51 +5,36 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         matrix = heights
-
-        # remove null cases
-        if not matrix or not matrix[0]:
-            return []
+        if not matrix or not matrix[0]: return []
 
         num_rows, num_cols = len(matrix), len(matrix[0])
-        pacific_q, atlantic_q = deque(), deque()
+        pq, aq = deque(), deque()
 
-        # add "coasts" (edges of matrix) to their respective oceans
         for i in range(num_rows):
-            pacific_q.append((i, 0))
-            atlantic_q.append((i, num_cols - 1))
+            pq.append((i, 0))
+            aq.append((i, num_cols - 1))
         for i in range(num_cols):
-            pacific_q.append((0, i))
-            atlantic_q.append((num_rows - 1, i))
+            pq.append((0, i))
+            aq.append((num_rows - 1, i))
 
         def bfs(q):
-            # will_flow_down will only contain coords that water can "flow down" from and to
             will_flow_down = set()
             while q:
-                (row, col) = q.popleft()
-                will_flow_down.add((row, col))
+                r, c = q.popleft()
+                will_flow_down.add((r, c))
 
-                # check neighbors using BFS
-                for (dir_r, dir_c) in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
-                    neighboring_r, neighboring_c = row + dir_r, col + dir_c
+                for (dr, dc) in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    nr, nc = r + dr, c + dc
+                    if not 0 <= nr < num_rows or not 0 <= nc < num_cols: continue
+                    if (nr, nc) in will_flow_down: continue
 
-                    # don't add if out of bounds
-                    if neighboring_r < 0 or neighboring_r >= num_rows :
-                        continue
-                    if neighboring_c < 0 or neighboring_c >= num_cols:
-                        continue
-
-                    # don't add if already added to set of flowable cells
-                    if (neighboring_r, neighboring_c) in will_flow_down:
-                        continue
-
-                    # don't add if neighboring cell is too short for water to flow down from
-                    if matrix[neighboring_r][neighboring_c]  < matrix[row][col]:
-                        continue
-
-                    q.append((neighboring_r, neighboring_c))
+                    # Remember: we start on the coast, and bfs toward the center
+                    if matrix[nr][nc] < matrix[r][c]: continue
+                    q.append((nr, nc))
             return will_flow_down
 
-        flow_to_pacific = bfs(pacific_q)
-        flow_to_atlantic = bfs(atlantic_q)
+        fp = bfs(pq)
+        fa = bfs(aq)
 
-        return flow_to_pacific.intersection(flow_to_atlantic)  # also write as "flow_to_pacific & flow_to_atlantic"
+        return fp & fa
+
