bfdcf707ead0060b9fc9ae67b20f191c93418da0
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-03-01 00:41:04 -0800

    commit

--- Tech Interview Handbook/Week 3 (Non-linear Data Structures)/BinaryTreeMaximumPathSum.py
+++ Tech Interview Handbook/Week 3 (Non-linear Data Structures)/BinaryTreeMaximumPathSum.py
@@ -12,19 +12,18 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         res = root.val
 
-        # return max path sum without split
         def dfs(root):
             nonlocal res
             if not root: return 0
 
-            left_max = dfs(root.left)
-            left_max = max(left_max, 0)
-            right_max = dfs(root.right)
-            right_max = max(right_max, 0)
+            # find maxes of each side (0 to address negative max)
+            left_max = max(dfs(root.left), 0)
+            right_max = max(dfs(root.right), 0)
 
             # compute max path sum WITH split
             res = max(res, root.val + left_max + right_max)
 
+            # return max path sum without split (WITH split is stored in res)
             return root.val + max(left_max, right_max)
 
         dfs(root)
