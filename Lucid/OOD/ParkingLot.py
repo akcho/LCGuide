ac3627b33a99a5b46f2b3ec66d093600fd849479
Andrew Cho <andrew@augmentedrealitycapture.com>
2022-03-02 13:24:24 -0800

    commit

--- Lucid/OOD/ParkingLot.py
+++ Lucid/OOD/ParkingLot.py
@@ -39,7 +39,7 @@ class Person():
 '''
 Users
 '''
-class Account:
+class Account(ABC):
   def __init__(self, user_name, password, person, status=AccountStatus.Active):
     self.__user_name = user_name
     self.__password = password
@@ -56,7 +56,7 @@ class Admin(Account):
   def add_parking_floor(self, floor): None
   def add_parking_spot(self, floor_name, spot): None
   def add_parking_display_board(self, floor_name, display_board): None
-  def add_customer_info_panel(self, floor_name, info_panel): Non
+  def add_customer_info_panel(self, floor_name, info_panel): None
   def add_entrance_panel(self, entrance_panel): None
   def add_exit_panel(self, exit_panel): None
 
--- Lucid/OOD/Test.py
+++ Lucid/OOD/Test.py
@@ -1,42 +1,96 @@
 from abc import ABC
-'''
-Enums and constants
-'''
+
+
 class VehicleType(Enum):
-    SUV, ELECTRIC, COMPACT = 1,2,3
-
-class ParkingSpotType(Enum):
-    HANDICAP, COMPACT, LARGE, MOTORBIKE: 1,2,3,4
-
-class AccountStatus(Enum):
-    ACTIVE, BLOCKED, ARCHIVED = 1,2,3
-
-class ParkingTicketStatus(Enum):
-    ACTIVE, PAID, PENDING = 1,2,3
-
-class Person:
-    def __init__(self, name, address, email, phone):
-    self.__name = name
-    self.__address = address
-    self.__email = email
-    self.__phone = phone
+    CAR, TRUCK, VAN, MOTORCYCLE = 1,2,3,4
 
 
-'''
-Users
-'''
-class Account:
-    def __init__(self, username, pw, person, is_active):
-        self.__username = username
-        self.__password = pw
-        self.__account_status = is_active
+class SpotType(Enum):
+    COMPACT, LARGE, HANDICAPPED, MOTORCYCLE, ELECTRIC = 1,2,3,4,5
 
-class Admin(Account):
-    def __init__(self, username, pw, person, is_active):
-        super().__init(username, pw, person, is_active)
+class Status(Enum):
+    ACTIVE, PENDING, INACTIVE = 1,2,3
 
-    def add_parking_floor(self, floor_num): None
-    def add_parking_spot(self, floor_num, spot_num): None
-    def add_parking_display_hub(self, floor_num, ID): None
-    def add_customer_info_p
 
+class ParkingStructure:
+    def __init__(self):
+        self.__active_tickets = {}
+        self.__floors = {}
+        self.__payment_portals = {}
+        self.__display_boards = {}
+
+
+class ParkingFloor:
+    def __init__(self):
+        self.__total_spots = None
+        self.__compact_spots= None
+        self.__large_spots = None
+        self.__handicapped_spots = None
+        self.__motorcycle_spots = None
+        self.__electric_spots = None
+        self.__payment_portal = PaymentPortal()
+        self.__display_board = DisplayBoard()
+
+
+class DisplayBoard:
+    def __init__(self):
+        self.__floor = None
+        self.__spot_to_display = None
+
+    def set_spot_to_display(self, spot_ID):
+        self.__spot_to_display = spot_ID
+
+    def get_free_spots(self):
+        # do this loop for each vehicle type:
+            # first check if there's a free spot on this floor
+            # if not, display free spot on an adjacent floor
+        None
+
+
+class PaymentPortal:
+    def __init__(self):
+        self.__ticket_db = {}
+
+    def process_ticket(self, ticket):
+        ticket_id = ticket.get_id()
+        if self.__ticket_db[id] == ACTIVE:
+            self.__ticket.set_status(INACTIVE)
+            self.__ticket_db.update()
+
+
+class ParkingSpot(ABC):
+    def __init__self(type):
+        self.__type = SpotType.type
+        self.__is_free = True
+
+    def set_free(self, status: Bool): self.__is_free = status
+    def is_free(self): return self.__is_free
+
+class Vehicle(ABC):
+    def __init__(self, type):
+        self.__type = VehicleType.type
+
+
+class Ticket:
+    def __init__(self):
+        self.__ID = None
+        self.__price = None
+        self.__dt = None
+        self.__status = INACTIVE
+
+    def get_amount_due(self):
+        curr_dt = None
+        hours_accrued = curr_dt - self.__dt
+        if hours_accroud == 1: return 4
+        elif hours_accrued == 2: return 4 + 3.5
+        elif hour_accrued == 3: return 4 + 3.5 + 3.5
+        else: return 4 + 3.5 + 3.5 + (2.5 * (hours_accrued-3))
+
+
+
+
+
+
+class ElectricPanel:
+    def __init__(self):
+        self.__id = None
